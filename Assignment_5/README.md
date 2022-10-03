bda9198facbb993f1b695b2b9ea49d002a2648a8
vaidehijagtap <112371822+vaidehijagtap@users.noreply.github.com>
2022-10-02 20:44:17 -0700

    CS648: Assignment - 5
    CS648: Assignment - 5

---
+++ Assignment_5/README.md
@@ -0,0 +1,8 @@
+# Assignment-5
+
+I have implemented following concepts in the website:
+* Configure Gulp file for SASS script
+* Creating SASS mixins
+* Sass nested rule to navigate menu and child elements
+* SCSS files: _variables.scss, _navigation.scss, _mixins.scss, _base.scss
+
---
+++ Assignment_5/gulpfile.js
@@ -0,0 +1,17 @@
+var{ src, dest, watch} = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+sass.compiler = require('node-sass');
+
+
+function css() {
+        return src('src/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+
+};
+
+//exports.default = function(){
+//watch('src/*.scss', css);
+//}
+
+exports.default = css;
